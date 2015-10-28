a026f624310e8af8c3421a537e0a179a4aabde18
Saif Niaz <saifniaz.3@gmail.com>
2015-10-28 10:58:21 -0400

    commited by Armin

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,10 @@
+reader = open("gadsby_stripped.txt", "r")
+String = reader.read()
+def has_no_e(String):
+   index = true
+   i = 0
+   while i < len(string):
+      if String(i) == "e": 
+        index = False
+   return index 
+reader.close()
---
+++ Midterm/strip.sh
@@ -0,0 +1,11 @@
+
+#! 
+
+#MD. Saif Niaz
+
+#100555440
+
+
+echo $file | head -n -m |tail -n +k
+
+
